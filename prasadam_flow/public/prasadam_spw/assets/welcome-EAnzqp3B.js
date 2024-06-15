d7916c83b1ab4f0650eb5f9cbc430f98d52ae55f
hkmj-erp <it@harekrishnajaipur.org>
2024-06-15 16:33:50 +0530

    misc

---
+++ prasadam_flow/public/prasadam_spw/assets/welcome-EAnzqp3B.js
@@ -0,0 +1,1 @@
+import{j as r,H as e}from"./index-DwYKWxFG.js";import{C as o}from"./chunk-FAWTVNS3-pJS65PGy.js";const t=()=>r.jsx(o,{children:r.jsx(e,{children:"Hare Krishna! Welcome to Prasadam Flow."})});export{t as Component};
